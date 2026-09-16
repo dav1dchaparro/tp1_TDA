da416a27c1f930edbe20822dcc98325771d7e598
Nicolas Llosas <nllosas@fi.uba.ar>
2026-09-16 20:10:53 +0000

    ajustes a codigo principal y nuevos graficos

--- monedas.py
+++ monedas.py
@@ -4,25 +4,25 @@ import sys
 def juego_monedas(monedas):
     i = 0
     k = len(monedas) - 1
-    suma_sophia = 0
+    suma_sofia = 0
     suma_mateo = 0
     movimientos = []
-    monedas_sophia = []
+    monedas_sofia = []
     monedas_mateo = []
-    contador = 0
+    turno_de_sophia = True
     while i <= k:
-        if contador % 2 == 0:  # Turno de Sophia
+        if turno_de_sophia:
             if monedas[i] > monedas[k]:
-                suma_sophia += monedas[i]
-                monedas_sophia.append(monedas[i])
+                suma_sofia += monedas[i]
+                monedas_sofia.append(monedas[i])
                 movimientos.append("Primera moneda para Sophia")
                 i += 1
             else:
-                suma_sophia += monedas[k]
-                monedas_sophia.append(monedas[k])
+                suma_sofia += monedas[k]
+                monedas_sofia.append(monedas[k])
                 movimientos.append("Última moneda para Sophia")
                 k -= 1
-        else:                  # Turno de Mateo (Sophia elige la más chica)
+        else:
             if monedas[i] < monedas[k]:
                 suma_mateo += monedas[i]
                 monedas_mateo.append(monedas[i])
@@ -33,8 +33,8 @@ def juego_monedas(monedas):
                 monedas_mateo.append(monedas[k])
                 movimientos.append("Última moneda para Mateo")
                 k -= 1
-        contador += 1
-    return suma_sophia, suma_mateo, movimientos, monedas_sophia, monedas_mateo
+        turno_de_sophia = not turno_de_sophia
+    return suma_sofia, suma_mateo, movimientos, monedas_sofia, monedas_mateo
 
 
 def leer_monedas(nombre_archivo):
@@ -52,12 +52,12 @@ def leer_monedas(nombre_archivo):
 if __name__ == "__main__":
     for nombre_archivo in sys.argv[1:]:
         monedas = leer_monedas(nombre_archivo)
-        suma_sophia, suma_mateo, movimientos, monedas_sophia, monedas_mateo = juego_monedas(monedas)
+        suma_sofia, suma_mateo, movimientos, monedas_sofia, monedas_mateo = juego_monedas(monedas)
 
         print(nombre_archivo)
         print("; ".join(movimientos))
-        print("Monedas de Sophia:", monedas_sophia)
+        print("Monedas de Sophia:", monedas_sofia)
         print("Monedas de Mateo:", monedas_mateo)
-        print("Ganancia de Sophia:", suma_sophia)
+        print("Ganancia de Sophia:", suma_sofia)
         print("Ganancia de Mateo:", suma_mateo)
         print()
